6ec696094f2f19b6338f21669fcea185c03883b3
Rick Kuilman <rickkuilman@gmail.com>
2018-10-14 20:33:53 +0200

    Updated CategoryGroup Factory

--- database/factories/CategoryGroupFactory.php
+++ database/factories/CategoryGroupFactory.php
@@ -4,6 +4,7 @@ use Faker\Generator as Faker;
 
 $factory->define(App\CategoryGroup::class, function (Faker $faker) {
     return [
+        'budget_id' => factory(\App\Budget::class)->create()->id,
         'name' => $faker->unique()->randomElement([
             'Fixed Expenses',
             'True Expenses',
