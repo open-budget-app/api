4e492265be36ba8f27189bbcf6caeb4408b76597
Rick Kuilman <rickkuilman@gmail.com>
2018-10-20 17:32:17 +0200

    Updated Login validation so email has to be maximum 255 chars

--- app/Http/Requests/LoginUserRequest.php
+++ app/Http/Requests/LoginUserRequest.php
@@ -24,7 +24,7 @@ class LoginUserRequest extends FormRequest
     public function rules()
     {
         return [
-            'email' => 'required',
+            'email' => 'required|max:255',
             'password' => 'required',
         ];
     }
