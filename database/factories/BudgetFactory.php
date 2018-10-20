04be59256697e964b7f1f745bba9202144c600b2
Rick Kuilman <rickkuilman@gmail.com>
2018-10-20 17:35:45 +0200

    Updated budget factory to supply a now required user id

--- database/factories/BudgetFactory.php
+++ database/factories/BudgetFactory.php
@@ -4,6 +4,7 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Budget::class, function (Faker $faker) {
     return [
+        'user_id' => factory(\App\User::class)->create()->id,
         'name' => 'Budget of ' . $faker->unique()->firstName()
     ];
 });
