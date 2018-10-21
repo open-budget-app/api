42e87ce601b8214cc089c87691997443b447edc5
ruudz <ruudzonnenberg@gmail.com>
2018-10-21 12:09:58 +0000

    Added PayeeStoreRequest.

---
+++ app/Http/Requests/PayeeStoreRequest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rule;
+
+class PayeeStoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'account_id' => 'sometimes|exists:accounts,id',
+            'name' => [
+                'required',
+                Rule::unique('payees')->where(function ($query) {
+                    return $query->where([
+                        'budget_id' => $this->route('budget')->id,
+                        'name' => $this->input('name'),
+                    ]);
+                })
+            ]
+        ];
+    }
+}
